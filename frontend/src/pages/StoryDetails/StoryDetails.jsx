341d9f6232b4e6220a884aab10661ab3d8a4e41d
Ashique <ashique9496@gmail.com>
2024-08-24 18:35:15 +0530

    Story_details page design completed

--- frontend/src/pages/StoryDetails/StoryDetails.jsx
+++ frontend/src/pages/StoryDetails/StoryDetails.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import "./StoryDetails.scss"
+import React from "react";
+import NavBar from "../../components/NavBar/NavBar"
+import "./StoryDetails.scss";
+import Footer from "../../components/Footer/Footer";
 
 const StoryDetails = () => {
-  return (
-    <div className="container">
-        <div className="row">
-            <div className="col-md-6"></div>
-            <div className="col-md-6"></div>
-        </div>
-    </div>
-  )
-}
+    return (
+        <>
+        <NavBar/>
+        <div className="container-fluid details_page">
+            <div className="row story_details_page">
+                <div className="col-md-6">
+                    <div className="story_img">
+                        <img src="\Images\static\CarouselBook2.jpg" alt="" />
+                    </div>
+                </div>
+                <div className="col-md-6">
+                    <div className="story_info">
+                        <h6>5 Contibution left</h6>
+                        <h3>Title</h3>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. In est illo odio exercitationem
+                            tempora, cum doloremque earum! Vitae adipisci, maiores commodi laudantium error animi nesciunt
+                            culpa aut dolor consequatur sapiente?
+                        </p>
 
-export default StoryDetails
+                        <button className="btn">CONTRIBUTE</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <Footer/>
+        </>
+    );
+};
+
+export default StoryDetails;
